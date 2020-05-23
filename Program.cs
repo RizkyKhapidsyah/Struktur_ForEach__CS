4427deef949b2b7288e1d7d7ebb3e665a1c44bc0
Rizky Khapidsyah <rizkyhafitsyah@gmail.com>
2020-05-24 00:35:54 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Struktur_ForEach__CS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] Stringarray = { "Satu", "Dua", "Tiga" };
+
+            foreach (String element in Stringarray)
+            {
+                Console.WriteLine(element + " ");
+            }
+
+            Console.Read();
+        }
+    }
+}
